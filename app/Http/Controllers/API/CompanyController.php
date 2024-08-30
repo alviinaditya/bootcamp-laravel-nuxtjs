fa7955079ee1d4ee367ab0aec513a23bd9cb049f
Alvin A. Rahman <alviinaditya@gmail.com>
2024-08-30 19:33:20 +0700

    Fix: create company without logo

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -55,7 +55,7 @@ class CompanyController extends Controller
             // Create Company
             $company = Company::create([
                 'name' => $request->name,
-                'logo' => $path,
+                'logo' => isset($path) ? $path : '',
             ]);
 
             if (!$company) {
